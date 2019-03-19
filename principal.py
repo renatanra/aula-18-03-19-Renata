189da0dc1b2945ef8d24c68e7f21a00f9421300e
Renata Nogueira Romano Anastacio <1802211@SALAS.AULAS>
2019-03-18 22:03:10 -0300

    alteração

---
+++ principal.py
@@ -0,0 +1,8 @@
+def somar(x,y):
+    return x+y
+
+def subtrai(x,y):
+    return x+y
+
+def mult():
+     pass
---
+++ teste.py
@@ -0,0 +1,6 @@
+
+import pytest
+from principal import somar
+from principal import subtrair
+def test_soma():
+    assert soma(2,4)==6
